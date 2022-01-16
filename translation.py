c0159e89cacf4dc32ad12a5369b4551b3d3d9ab7
KOTDEOMOREPOS <90854597+KOTDEOMOREPOS@users.noreply.github.com>
2022-01-16 09:56:13 +0530

    Add files via upload

--- translation.py
+++ translation.py
@@ -1,17 +1,17 @@
 class Translation(object):
-    START_TEXT = """ <b> 🙋‍♂️ Hi {} , I'm A Simple File Renamer+File To Video Converter Bot With Permanent Thumbnail support!💯</b> \n
+    START_TEXT = """ <b> 🙋‍♂️𝗛𝗘𝗟𝗟𝗢 {} ,𝗜'𝗠 𝗔 𝗦𝗜𝗠𝗣𝗟𝗘 𝗙𝗜𝗟𝗘 𝗥𝗘𝗡𝗔𝗠𝗘+ 𝗩𝗜𝗗𝗘𝗢 𝗖𝗢𝗡𝗩𝗧𝗘𝗥 𝗕𝗢𝗧 𝗪𝗜𝗧𝗛 𝗣𝗘𝗥𝗠𝗔𝗡𝗘𝗡𝗧 𝗧𝗛𝗨𝗠𝗕𝗡𝗔𝗜𝗟 𝗦𝗨𝗣𝗣𝗢𝗥𝗧! 💯</b> \n
 
-<b>Bot Maintained By: @Mo_Tech_YT </b> \n 
+<b>𝗕𝗢𝗧 𝗖𝗥𝗘𝗔𝗧𝗘𝗗 𝗔𝗡𝗗 𝗠𝗔𝗧𝗜𝗡𝗔𝗘𝗗 𝗕𝗬: @KOT_BOTS | @KOT_FREE_DE_LA_HOYA_OFF | @KOT_REPORS</b> \n 
 """
 
     RENAME_403_ERR = "What Are You Doing? Bruuh!!"
-    BANNED_USER_TEXT = "Sorry!! But Mai Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Contact : @No_OnE_Kn0wS_Me For more Details.. " 
+    BANNED_USER_TEXT = "Sorry!! But Mai Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Contact : @KOT_FREE_DE_LA_HOYA_OFF For more Details.. " 
     ABS_TEXT = "What Are You Trying To Do,Mate?"
-    UPGRADE_TEXT = "CONTACT @Mo_Tech_Group"
+    UPGRADE_TEXT = "CONTACT @KOT_FREE_DE_LA_HOYA_OFF"
     DOWNLOAD_START = "<b>Downloading To My server !! pls Wait</b>"
-    UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TG</b>"
+    UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TELEGRAM</b>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry Do You Think! I'll Upload It?"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me Support Our Channel @mai_bots</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me Support Our Channel @KOT_BOTS | @KOT_REPORS</b>"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
     NOT_AUTH_USER_TEXT = "CONTACT @Mo_Tech_Group"
     NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @MRK_YT "
@@ -26,8 +26,24 @@ class Translation(object):
     NO_THUMB_FOUND = "No Thumbnail found"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
-    ABOUT_ME = """<b>Meh : \n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n i can also convert files into videos\n\n🗣️ Support Group @Mo_Tech_Group\n🤖 Bot Updates @Mo_Tech_YT\n\n🤖Bot Creater @MaI_BoTs\n👨‍💻Editing @MRK_YT</b>"""
-    HELP_USER = """Checkout The Available Commands Here \n\n Do Follow @Mo_Tech_YT If You Find This Bot Usefull❤️"""
+    ABOUT_ME = """╭──────[@KOT_BOTS]───────〄
+│
+├ Nᴀᴍᴇ : <a href='https://t.me/KOT_RENAME_V2_BOT'>Kᴏᴛ Rᴇɴᴀᴍᴇ + Vɪᴅᴇᴏ Cᴏɴᴠᴛᴇʀ Bᴏᴛ</a>
+│
+├ Sᴇʀᴠᴇʀ : <a href='https://heroku.com/'>Hᴇʀᴏᴋᴜ</a>
+│ 
+├ Lᴀɴɢᴜᴀɢᴇ : <a href='https://docs.pyrogram.org/'>Pʏᴛʜᴏɴ 3.9.6</a>
+│
+├ Vᴇʀꜱɪᴏɴ : <a href='https://t.me/KOT_RENAME_V2_BOT'>1.5.0 Bᴇᴛᴀ</a>
+│
+├ Fʀᴀᴍᴇᴡᴏʀᴋ : <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ 1.2.9</a>
+│
+├ Dᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/KOT_FREE_DE_LA_HOYA_OFF'>✯°• Kᴏᴛ Fʀᴇᴇ Dᴇ Lᴀ Hᴏʏᴀ Oғғ °•✯ | ✪ Bᴏᴛs CʀᴇᴀᴛᴏR ✪</a>
+│
+├ Pᴏᴡᴇʀᴇᴅ Bʏ : <a href='https://t.me/KOT_LINKS_TEAM'>Kᴏᴛ Lɪɴᴋs Tᴇᴀᴍ</a>
+│
+├ Uᴘᴅᴀᴛᴇᴅ Oɴ : [ 16.1.2022 ] 05:00 AM"""
+    HELP_USER = """Checkout The Available Commands Here \n\n Do Follow @KOT_BOTS | @KOT_REPORS If You Find This Bot Usefull❤️"""
     RENAME_HELP = """Here are The Available Commands In Reanme \n\n\n▪️ <code>/rename</code> : Reply To An File/video With <code>/rename Filename.extension</code> For Renaming"""
     C2V_HELP = """Here Are The Available Commands In File To Video \n\n\n ▪️<code>/c2v</code> : Reply To An File With /c2v To Convert It Into Video"""
     THUMBNAIL_HELP = """Here Are The Available Commands In Custom Thumbnail \n\n\n ▪️ Send A Photo To Set The Custom Thumbnail \n▪️ <code>/showthumb</code> : For Checking The Current Thumbnail \n▪️<code>/delthumb</code> : For Deleting The Current Saved Thumbnail"""
